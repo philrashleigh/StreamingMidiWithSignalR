8fb5017d50e41c2e5b038cad4f63e6b72428983f
Philip Rashleigh <philip.rashleigh@audacia.co.uk>
2019-09-30 16:38:25 +0100

    Added js files to repo

---
+++ src/PhilipRashleigh.StreamingMidi.Client/wwwroot/js/receiver.js
@@ -0,0 +1,64 @@
+﻿const MidiReceiver = (() => {
+    let connection = null;
+    let synth = null;
+    
+    let initSynth = () => {
+        synth = new Tone.PolySynth(4, Tone.FMSynth).toMaster();
+    };
+    
+    let midiToNoteName = (midiValue) => {
+        const chromatic = [ 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'G', 'Ab', 'A', 'Bb', 'B' ];
+        
+        let name = chromatic[midiValue % 12];
+        let oct = Math.floor(midiValue / 12) - 1;
+        
+        return name + oct;
+    };
+    
+    let toggle = (command, midiNote) => {
+        let note = midiToNoteName(midiNote);
+        
+        switch (command) {
+            case 144:
+                synth.triggerAttack(note);
+                break;
+            case 128:
+                synth.triggerRelease(note);
+                break;        
+        }
+    };
+    
+    let subscribeToStream = () => {
+        connection.on("Receive", (message) => {
+            let data = message.split(',').map(item => parseInt(item));
+            let [command, midiNote, _] = data;
+    
+            command = command & 0xf0;
+            
+            toggle(command, midiNote);
+        });
+    };
+    
+    let initAsync = async url => {
+        initSynth();
+        
+        let connectAsync = async function () {
+            connection = new signalR.HubConnectionBuilder()
+                .withUrl(url, {logger: signalR.LogLevel.Information})
+                .withAutomaticReconnect()
+                .build();
+
+            await connection.start();
+
+            console.log("Connected to SignalR Hub");          
+        };        
+        
+        await connectAsync();
+        
+        subscribeToStream();
+    };
+            
+    return {
+        initAsync: initAsync
+    };
+})();
---
+++ src/PhilipRashleigh.StreamingMidi.Client/wwwroot/js/sender.js
@@ -0,0 +1,46 @@
+﻿const MidiSender = (() => {
+    let connection = null;
+    
+    let onMidiMessage = (message, subject) => {
+        //Stream midi data to SingalR Hub
+        subject.next(message.data.toString());
+    };
+    
+    let subscribeInputs = (midiData, subject) => {
+        //Loop through all midi-controllers available and tie them to onMidiMessage event
+        for (let input of midiData.inputs.values()) {
+            input.onmidimessage = function(message) {
+                onMidiMessage(message, subject);
+            };
+        }
+    };
+    
+    let initAsync = async url => {
+        let connectAsync = async function () {
+            connection = new signalR.HubConnectionBuilder()
+                .withUrl(url, {logger: signalR.LogLevel.Information})
+                .withAutomaticReconnect()
+                .build();
+
+            await connection.start();
+
+            console.log("Connected to SignalR Hub");          
+        };        
+        
+        await connectAsync();
+
+        let subject = new signalR.Subject();
+        await connection.send("Send", subject);
+                
+        let midiData = await navigator.requestMIDIAccess({ 
+            //Include software devices
+            sysex: true
+        });
+        
+        subscribeInputs(midiData, subject);
+    };
+            
+    return {
+        initAsync: initAsync
+    };
+})();
